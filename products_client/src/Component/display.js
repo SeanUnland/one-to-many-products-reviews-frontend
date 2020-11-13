f512c459e99b73db3cd93b4ae6aa3fc38856ab0f
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-11-13 17:10:17 -0500

    edited frontend

--- products_client/src/Component/display.js
+++ products_client/src/Component/display.js
@@ -1,37 +1,6 @@
 import React from "react";
 
 const display = (props) => {
-  React.useEffect(() => {
-    const makeAPICall = async () => {
-      try {
-        const res = await res.json();
-        const formattedData = preparedData(json);
-        createChart(formattedData);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    makeAPICall();
-  });
-
-  const preparedData = (data) => {
-    const chartData = {
-      labels: [],
-      datasets: [{ label: "reviews", data: [] }],
-    };
-    data.products.forEach((product) => {
-      chartData.datasets[0].data.push(product.name);
-    });
-    return chartData;
-  };
-
-  const createChart = (data) => {
-    const ctx = document.querySelector("#products");
-    const productsChart = new Chart(ctx, {
-      type: "line",
-      data: data,
-    });
-  };
   return <div></div>;
 };
 
